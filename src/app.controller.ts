0bfc107015e957db969578e211f732f6427e4811
danrigoli <danterigoli02@gmail.com>
2024-09-28 23:50:22 -0300

    🐛 fix: replaced hello for health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHealthCheck(): string {
     return this.appService.getHello();
   }
 }
