8f865a216be9d277552aa7d87e19717d77155ef6
danrigoli <danterigoli02@gmail.com>
2024-10-29 01:03:58 -0300

    finishing migrations

---
+++ src/database/migrations/1730167257359-on-delete-payments.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class OnDeletePayments1730167257359 implements MigrationInterface {
+  name = 'OnDeletePayments1730167257359';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "payments" DROP CONSTRAINT "FK_4781cf05f36ba314cdd314c0c66"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "payments" DROP CONSTRAINT "FK_91fbd90e589339e9581a9cfad5b"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "payments" ADD CONSTRAINT "FK_4781cf05f36ba314cdd314c0c66" FOREIGN KEY ("company_id") REFERENCES "companies"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "payments" ADD CONSTRAINT "FK_91fbd90e589339e9581a9cfad5b" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "payments" DROP CONSTRAINT "FK_91fbd90e589339e9581a9cfad5b"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "payments" DROP CONSTRAINT "FK_4781cf05f36ba314cdd314c0c66"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "payments" ADD CONSTRAINT "FK_91fbd90e589339e9581a9cfad5b" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "payments" ADD CONSTRAINT "FK_4781cf05f36ba314cdd314c0c66" FOREIGN KEY ("company_id") REFERENCES "companies"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+  }
+}
---
+++ src/database/migrations/1730167474015-on-delete-payments.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class OnDeletePayments1730167474015 implements MigrationInterface {
+    name = 'OnDeletePayments1730167474015'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "payments" DROP CONSTRAINT "FK_4781cf05f36ba314cdd314c0c66"`);
+        await queryRunner.query(`ALTER TABLE "payments" DROP CONSTRAINT "FK_91fbd90e589339e9581a9cfad5b"`);
+        await queryRunner.query(`ALTER TABLE "payments" DROP COLUMN "deleted_at"`);
+        await queryRunner.query(`ALTER TABLE "payments" ADD CONSTRAINT "FK_4781cf05f36ba314cdd314c0c66" FOREIGN KEY ("company_id") REFERENCES "companies"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "payments" ADD CONSTRAINT "FK_91fbd90e589339e9581a9cfad5b" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "payments" DROP CONSTRAINT "FK_91fbd90e589339e9581a9cfad5b"`);
+        await queryRunner.query(`ALTER TABLE "payments" DROP CONSTRAINT "FK_4781cf05f36ba314cdd314c0c66"`);
+        await queryRunner.query(`ALTER TABLE "payments" ADD "deleted_at" TIMESTAMP`);
+        await queryRunner.query(`ALTER TABLE "payments" ADD CONSTRAINT "FK_91fbd90e589339e9581a9cfad5b" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "payments" ADD CONSTRAINT "FK_4781cf05f36ba314cdd314c0c66" FOREIGN KEY ("company_id") REFERENCES "companies"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+}
